a5f3563b929abdf12edaa67ef5351bdf93582884
Snyman <125444056+SnymanD@users.noreply.github.com>
2024-02-13 00:12:55 +0200

    Added more files
    I added more files

---
+++ to_do_list.py
@@ -0,0 +1,62 @@
+""" A To-Do List application is a useful project that helps users manage
+and organize their tasks efficiently
+This project aims to create a command-line or GUI-based application using Python, allowing
+users to create, update, and track their to-do lists
+"""
+from pickle import TRUE
+
+tasks = []
+
+
+def track_tasks():
+    if not tasks:
+        print("____Currently you do not have any tasks____")
+    else:
+        print("________Current Tasks________")
+        for index, task in enumerate(tasks):
+            print(f"{index}. {task}")
+
+
+def create_tasks():
+    task = input("Please Enter A New Task: ")
+    tasks.append(task)
+    print(f"'{task}' is added on your list.")
+
+
+def remove_tasks():
+    track_tasks()
+    try:
+        task_to_remove = int(input("Enter the number of the task to delete: "))
+        if 0 <= task_to_remove < len(tasks):
+            tasks.pop(task_to_remove)
+            print(f"{task_to_remove} has been deleted.")
+        else:
+            print(f"{task_to_remove} was not found.")
+    except:
+        print("Invalid input!")
+
+
+if __name__ == "__main__":
+
+    print("Welcome to To-Do-List app")
+    while TRUE:
+        print("")
+        print("Select One Of The Following Options: ")
+        print(f"{1}. List all the tasks")
+        print(f"{2}. Add a new task")
+        print(f"{3}. Delete a task")
+        print(f"{4}. Quit")
+
+        choice = int(input("Select a number: "))
+
+        if choice == 1:
+            track_tasks()
+        elif choice == 2:
+            create_tasks()
+        elif choice == 3:
+            remove_tasks()
+        elif choice == 4:
+            quit()
+
+else:
+    print("Bye, enjoy the rest of your day!")
