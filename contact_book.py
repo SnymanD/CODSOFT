d66e21295f908b68ae02d482ed44d1bacef9b183
Snyman <125444056+SnymanD@users.noreply.github.com>
2024-02-13 22:00:15 +0200

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -7,93 +7,85 @@
 # User Interface: Design a user-friendly interface for easy interaction.
 
 contacts = {}
-
-print("___________CONTACTS BOOK____________")
+print("__________CONTACTS BOOK__________")
 print("")
 
 
 def display_contacts():
-    print("____Contacts List____")
+    print("___Contacts List___")
     for key in contacts:
         print(f"{key}{contacts.get(key)}")
 
 
 while True:
+    print("Choose to perform an action: ")
+    choice = int(input(f"{1} List All The Contacts \n"
+                       f"{2} Add New Contact \n"
+                       f"{3} Search A Contact \n"
+                       f"{4} Update A Contact \n"
+                       f"{5} Delete A Contact \n"
+                       f"{6} Quit \n"
+                       ">>>"))
 
-    print("___Choose the option that you want___")
-    choice = input("1. Add New Contact \n "
-                   "2. Search Contact \n "
-                   "3. Lists Contacts \n "
-                   "4. Update Contact \n "
-                   "5. Delete Contact \n "
-                   "6. Quit \n"
-                   ">>>"
-                   )
-
-    if choice == "1":
-        name = input("Enter a name: ").capitalize()
-        email = input("Enter an email address: ")
-        address = input("Enter the home address: ")
-        phone_number = input("Enter the mobile numbers: ")
-        contacts["-"] = f"Name: {name}\n Phone Number: {phone_number} \n Email: {email} \n Address: {address} \n"
-        print(f"{name} is added to your contacts")
-        print("")
-
-    elif choice == "2":
-        search_contact = input("Enter the name of the contact: ")
-        if search_contact in contacts:
-            print(f"{search_contact}{contacts}")
-        else:
-            print("Contacts not found!")
-            print("")
-
-    elif choice == "3":
+    if choice == 1:
         if not contacts:
-            print("Contact book is empty")
+            print("Contact book is currently empty!")
+            print("")
         else:
             display_contacts()
+
+    elif choice == 2:
+        name = input("Please enter a name of the person: ").capitalize()
+        email = input("Please enter an email address of the person: ")
+        address = input("Please enter the home of address of the person: ")
+        mobile_number = int(input("Please enter the mobile number: "))
+        print(f"{name} is added to your contacts")
+        print("")
+        contacts["-"] = f"Name: {name}\n Email: {email}\n Address: {address}\n Mobile: {mobile_number}\n"
+        
+    elif choice == 3:
+        search_contact = input("Please enter the name of the contact to search: ").capitalize()
+        if search_contact not in contacts:
+            print("Contact name not found!")
+        else:
+            print(contacts[search_contact])
             print("")
 
-    elif choice == "4":
-        while True:
-            update_contact = input("Enter the name of the contact you want to update: ").capitalize()
-            if update_contact in contacts:
-                choose = int(input(f"{1}. Update email \n "
-                                   f"{2}. Update address \n"
-                                   f"{3}. Update phone numbers \n"
-                                   f"{4}. Quit \n"
-                                   ">>>"))
-                if choose == 1:
-                    new_email = input("Enter a new email: ").capitalize()
-                    contacts[new_email] = new_email
-                    print("Email Address Updated")
-                    display_contacts()
+    elif choice == 4:
+        update_contact = input("Please enter the name of the contact to update: ").capitalize()
+        if update_contact in contacts:
+            print("What would you like to do?")
+            choose = int(input(f"{1} Change the email address \n"
+                               f"{2} Change the home address \n"
+                               f"{3} Change the mobile number \n"
+                               f"{4} Update the name \n"
+                               f"{5} Quit \n"))
+            if choose == 1:
+                new_email = input("Please enter a new email address:")
+                contacts[0] = new_email
+                display_contacts()
+            elif choose == 2:
+                address = input("Please enter a new email address:")
+                contacts[address] = address
+                display_contacts()
+            elif choose == 3:
+                mobile_number = int(input("Enter a new mobile number: "))
+                contacts[mobile_number] = mobile_number
+                display_contacts()
+            elif choose == 4:
+                name = input("Please enter a new name: ").capitalize()
+                contacts[name] = name
+                display_contacts()
+            elif choose == 5:
+                break
 
-                elif choose == 2:
-                    address = input("Enter New Address: ")
-                    contacts[address] = address
-                    print("Address Updated")
-
-                elif choose == 3:
-                    phone_number = input("Enter New Phone Numbers: ")
-                    contacts[phone_number] = phone_number
-                    print("Phone Numbers Updated")
-
-                elif choose == 4:
-                    break
+        elif choice == 5:
+            delete_contact = input("Enter the name of the contact to delete: ").capitalize()
+            if delete_contact in contacts:
+                contacts.pop(delete_contact)
+                display_contacts()
             else:
                 print("Contact not found!")
 
-    elif choice == "5":
-        delete_contact = input("Enter The Name Of The Contact To Delete")
-        if delete_contact in contacts:
-            contacts.pop(delete_contact)
-        else:
-            print("Contact not Found!")
-
-    elif choice == "6":
+    elif choice == 6:
         break
-    if choice != ("1" or "2" or "3" or "4" or "5" or "6"):
-        print("Invalid Choice!")
-
-print("Goodbye")
