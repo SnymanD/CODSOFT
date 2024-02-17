169015742960e44fb1e5eb901b06b12e47ed37bf
SnmanD <dhlaminisnyman@gmail.com>
2024-02-17 06:51:47 +0200

    fixed some bugs

--- tasks/password_generator.py
+++ tasks/password_generator.py
@@ -1,8 +1,4 @@
-"""
-A password generator is a useful tool that generates strong and
-random passwords for users. This project aims to create a
-password generator application using Python, allowing users to
-specify the length and complexity of the password.
+"""A password generator is a useful tool that generates strong and random passwords for users. This project aims to create a password generator application using Python, allowing users to specify the length and complexity of the password.
 """
 # User Input: Prompt the user to specify the desired length of the password.
 # Generate Password: Use a combination of random characters to generate a password of the specified length.
